bd52b5ca9d4069163f46e4ed567d234f348635e8
Mikael Jorda <mikael.jorda@gmail.com>
2024-10-27 18:23:46 -0700

    add example with moving object with friction

--- python_examples/kuka_move_object_with_friction.py
+++ python_examples/kuka_move_object_with_friction.py
@@ -8,11 +8,12 @@ from dataclasses import dataclass
 
 DEG_TO_RAD = math.pi / 180.0
 
-
 class State(Enum):
     INIT = auto()
-    GOING_TO_CONTACT = auto()
-    SURFACE_ALIGNMENT = auto()
+    GOING_ABOVE_OBJECT = auto()
+    GOING_TO_OBJECT = auto()
+    MOVE_OBJECT = auto()
+    END = auto()
 
 
 @dataclass
@@ -23,20 +24,14 @@ class RedisKeys:
     kuka_current_orientation: str = "opensai::controllers::Kuka::eef_control::eef_task::current_orientation"
     kuka_sensed_force: str = "opensai::controllers::Kuka::eef_control::eef_task::sensed_force"
     kuka_desired_force: str = "opensai::controllers::Kuka::eef_control::eef_task::desired_force"
-    kuka_desired_moment: str = "opensai::controllers::Kuka::eef_control::eef_task::desired_moment"
     kuka_force_space_dimension: str = "opensai::controllers::Kuka::eef_control::eef_task::force_space_dimension"
-    kuka_moment_space_dimension: str = "opensai::controllers::Kuka::eef_control::eef_task::moment_space_dimension"
     kuka_force_space_axis: str = "opensai::controllers::Kuka::eef_control::eef_task::force_space_axis"
-    kuka_moment_space_axis: str = "opensai::controllers::Kuka::eef_control::eef_task::moment_space_axis"
-    kuka_closed_loop_force_control: str = "opensai::controllers::Kuka::eef_control::eef_task::closed_loop_force_control"
-    kuka_otg_enabled: str = "opensai::controllers::Kuka::eef_control::eef_task::otg_enabled"
     config_file_name: str = "::sai2-interfaces-webui::config_file_name"
-    table_goal_angles: str = "opensai::controllers::Table::tilt_controller::tilt_task::goal_position"
-
+    object_pose = "opensai::sensors::Object::object_pose"
 
 redis_keys = RedisKeys()
 
-config_file_for_this_example = "kuka_plate_table.xml"
+config_file_for_this_example = "kuka_object_friction.xml"
 
 init_goal_pos = np.array([0.6, 0.0, 0.575])
 init_goal_ori = np.array([[-1.0, 0, 0], [0, 1.0, 0], [0, 0, -1.0]])
@@ -59,14 +54,13 @@ if config_file_name != config_file_for_this_example:
 
 # set the initial goal position and orientation
 redis_client.set(redis_keys.kuka_goal_position,
-                 json.dumps(init_goal_pos.tolist()))
+                 json.dumps(goal_pos.tolist()))
 redis_client.set(redis_keys.kuka_goal_orientation,
                  json.dumps(init_goal_ori.tolist()))
 
 # loop at 100 Hz
 loop_time = 0.0
 dt = 0.01
-loop_step = 0
 state = State.INIT
 
 time.sleep(0.01)
@@ -78,19 +72,6 @@ try:
         time.sleep(
             max(0, loop_time - (time.perf_counter_ns() * 1e-9 - init_time)))
 
-        # table tilt control
-        if (loop_step % 1000) == 0:
-            table_goal_angles[0] = 12.0 * DEG_TO_RAD
-        if (loop_step % 1000) == 500:
-            table_goal_angles[0] = -12.0 * DEG_TO_RAD
-        if (loop_step % 1500) == 0:
-            table_goal_angles[1] = 18.0 * DEG_TO_RAD
-        if (loop_step % 1500) == 750:
-            table_goal_angles[1] = -18.0 * DEG_TO_RAD
-
-        redis_client.set(redis_keys.table_goal_angles,
-                         json.dumps(table_goal_angles.tolist()))
-
         # read robot state
         current_position = np.array(
             json.loads(redis_client.get(redis_keys.kuka_current_position)))
@@ -100,42 +81,64 @@ try:
         # state machine
         if state == State.INIT:
             # monitor error
-            pos_error = np.linalg.norm(init_goal_pos - current_position)
-            ori_error = np.linalg.norm(init_goal_ori - current_orientation)
+            pos_error = np.linalg.norm(goal_pos - current_position)
+            ori_error = np.linalg.norm(goal_ori - current_orientation)
             if pos_error < 1e-2 and ori_error < 1e-2:
-                contact_goal_position = init_goal_pos - np.array([0, 0, 0.4])
+                object_position = np.array(json.loads(redis_client.get(redis_keys.object_pose)))[0:3,3]
+                goal_pos = object_position + np.array([0, 0, 0.1])
                 redis_client.set(redis_keys.kuka_goal_position,
-                                 json.dumps(contact_goal_position.tolist()))
-                state = State.GOING_TO_CONTACT
-                print("Going to contact")
+                                 json.dumps(goal_pos.tolist()))
+                state = State.GOING_ABOVE_OBJECT
+                print("Going above object")
 
-        elif state == State.GOING_TO_CONTACT:
+        elif state == State.GOING_ABOVE_OBJECT:
+            pos_error = np.linalg.norm(goal_pos - current_position)
+            if(pos_error < 1e-2):
+                goal_pos = current_position - np.array([0, 0, 0.15])
+                redis_client.set(redis_keys.kuka_goal_position,
+                                 json.dumps(goal_pos.tolist()))
+                state = State.GOING_TO_OBJECT
+                print("Going to object")
+            
+        elif state == State.GOING_TO_OBJECT:
             # monitor contact force
             sensed_force = np.array(
                 json.loads(redis_client.get(redis_keys.kuka_sensed_force)))
-            if sensed_force[2] < -15.0:
+            if sensed_force[2] < -12.0:
                 # parametrize force space and moment space
                 redis_client.set(redis_keys.kuka_force_space_dimension, "1")
                 redis_client.set(redis_keys.kuka_force_space_axis,
                                  json.dumps([0, 0, 1]))
-                redis_client.set(redis_keys.kuka_moment_space_dimension, "2")
-                redis_client.set(redis_keys.kuka_moment_space_axis,
-                                 json.dumps([0, 0, 1]))
-                redis_client.set(redis_keys.kuka_closed_loop_force_control, "1")
+                redis_client.set(redis_keys.kuka_desired_force, json.dumps([0, 0, -12]))
 
-                redis_client.set(redis_keys.kuka_otg_enabled, "0")
+                # wait more than 2 controller cycles to let the inputs reset after force control reparametrization
+                time.sleep(0.002)
 
-                state = State.SURFACE_ALIGNMENT
-                print("Surface alignment")
+                # move object towards the robot in the x direction
+                goal_pos = current_position - np.array([0.1, 0, 0])
+                redis_client.set(redis_keys.kuka_goal_position,
+                                 json.dumps(goal_pos.tolist()))
 
-        elif state == State.SURFACE_ALIGNMENT:
-            # send the force and moment goals
-            redis_client.set(redis_keys.kuka_desired_force,
-                             json.dumps([0, 0, -15.0]))
-            redis_client.set(redis_keys.kuka_desired_moment,
-                             json.dumps([0, 0, 0]))
+                state = State.MOVE_OBJECT
+                print("Move object")
 
-        loop_step += 1
+        elif state == State.MOVE_OBJECT:
+            pos_error = np.linalg.norm(goal_pos[0:2] - current_position[0:2])
+            if(pos_error < 1e-3):
+                redis_client.set(redis_keys.kuka_force_space_dimension, "0")
+                
+                # wait more than 2 controller cycles to let the inputs reset after force control reparametrization
+                time.sleep(0.002)
+                
+                redis_client.set(redis_keys.kuka_goal_position,
+                                 json.dumps(init_goal_pos.tolist()))
+                state = State.END
+                print("Return to initial position")
+            
+
+        elif state == State.END:
+            print("Exit state machine")
+            exit(0)
 
 except KeyboardInterrupt:
     print("Keyboard interrupt")
